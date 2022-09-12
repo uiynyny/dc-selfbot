61a1ec90b4ef98fc01f34183813a5b0d681f0253
Yan Zhang <dexz@google.com>
2022-09-11 21:32:51 -0400

    add fupan hook

--- app.py
+++ app.py
@@ -14,10 +14,7 @@ hooks = {
     'TA': [os.environ["TA_CN"], os.environ["TA"]],
     'TR': [os.environ["VIVIAN_TRADE_CN"], os.environ["VIVIAN_TRADE"]],
     'DAILY': [os.environ["VIVIAN_DAILY_CN"], os.environ["VIVIAN_DAILY"]],
-    # 'KUO': [os.environ['FUPAN_HOOK'] + '?thread_id=998336868913528944'],
-    # 'SKRRA': [os.environ['FUPAN_HOOK'] + '?thread_id=1006216000586403981'],
-    # 'QIE': [os.environ['FUPAN_HOOK'] + '?thread_id=1006216102856118362'],
-    # 'DORCAS': [os.environ['FUPAN_HOOK'] + '?thread_id=1006215829114851328']
+    'SIGNAL': [os.environ['SIGNAL_HOOK']],
 }
 
 CHANNELS = {
@@ -38,10 +35,10 @@ CHANNELS = {
     '899394668662505502': {'description': "DAILY-ETH", 'hooks': hooks['DAILY']},
     '930970372726222898': {'description': "DAILY-NEWS", 'hooks': hooks['DAILY']},
     # coop
-    # '1004963672826847313': {'description': 'KUO', 'hooks': hooks['KUO']},
+    '1004963672826847313': {'description': 'KUO', 'hooks': hooks['SIGNAL']},
     # '913054223460102205': {'description': "SILVERBAY_ANNOUNCE", 'hooks': hooks['SILVERBAY_ANNOUNCE']},
     # '1000276250255949844': {'description': 'SKRRA', 'hooks': hooks['SKRRA']},
-    # '992093748278341672': {'description': 'QIE', 'hooks': hooks['QIE']},
+    '992093748278341672': {'description': 'QIE', 'hooks': hooks['SIGNAL']},
     # '1006214862151635006': {'description': 'DORCAS', 'hooks': hooks['DORCAS']}
 }
 
@@ -60,10 +57,10 @@ def on_message(resp):
         guildID = m["guild_id"] if "guild_id" in m else None
         if guildID == "895412557668548608":
             blockchain_sniper(m)
-        # elif guildID == '913012487841001502':
-        #     silverbay(m)
-        # elif guildID == '965110788618588200':
-        #     kolunite(m)
+        elif guildID == '913012487841001502':
+            silverbay(m)
+        elif guildID == '965110788618588200':
+            kolunite(m)
 
 
 def kolunite(m):
